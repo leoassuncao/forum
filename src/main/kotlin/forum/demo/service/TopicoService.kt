37d71acfcb9118fa4cc6142199d0e92ec85d23cb
Leonardo Assunção <leonardo.assuncao@aisdigital.com.br>
2023-08-08 15:10:26 +0200

    adding code responses

--- src/main/kotlin/forum/demo/service/TopicoService.kt
+++ src/main/kotlin/forum/demo/service/TopicoService.kt
@@ -30,25 +30,28 @@ class TopicoService(
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(form: NovoTopicoForm) {
+    fun cadastrar(form: NovoTopicoForm): TopicoView {
         val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+        return topicoViewMapper.map(topico)
     }
 
-    fun atualizar(form: AtualizacaoTopicoForm) {
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
         val topico = topicos.stream().filter { t ->
             t.id == form.id
         }.findFirst().get()
-        topicos = topicos.minus(topico).plus(Topico(
-                    id = form.id,
-                    titulo = form.titulo,
-                    mensagem = form.mensagem,
-                    autor = topico.autor,
-                    curso = topico.curso,
-                    respostas = topico.respostas,
-                    status = topico.status
-        ))
+        val topicoAtualizado = Topico(
+            id = form.id,
+            titulo = form.titulo,
+            mensagem = form.mensagem,
+            autor = topico.autor,
+            curso = topico.curso,
+            respostas = topico.respostas,
+            status = topico.status
+        )
+        topicos = topicos.minus(topico).plus(topicoAtualizado)
+        return topicoViewMapper.map(topicoAtualizado)
     }
 
     fun deletar(id: Long) {
